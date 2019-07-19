27373c5e2b495924585d23edd6bd600fbb084da5
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-07-20 02:20:47 +0900

    add createComponent method

--- my_yii.php
+++ my_yii.php
@@ -27,4 +27,51 @@ if(!class_exists('YiiBase', false))
  */
 class Yii extends YiiBase
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function createComponent($config)
+    {
+        $args = func_get_args();
+        if(is_string($config))
+        {
+            $type=$config;
+            $config=array();
+        }
+        elseif(isset($config['class']))
+        {
+            $type=$config['class'];
+            unset($config['class']);
+        }
+        else
+            throw new CException(Yii::t('yii','Object configuration must be an array containing a "class" element.'));
+
+        if(!class_exists($type,false))
+            $type=Yii::import($type,true);
+
+        if(($n=func_num_args())>1)
+        {
+            if($n===2)
+                $object=new $type($args[1]);
+            elseif($n===3)
+                $object=new $type($args[1],$args[2]);
+            elseif($n===4)
+                $object=new $type($args[1],$args[2],$args[3]);
+            else
+            {
+                unset($args[0]);
+                $class=new ReflectionClass($type);
+                // Note: ReflectionClass::newInstanceArgs() is available for PHP 5.1.3+
+                // $object=$class->newInstanceArgs($args);
+                $object=call_user_func_array(array($class,'newInstance'),$args);
+            }
+        }
+        else
+            $object=new $type;
+
+        foreach($config as $key=>$value)
+            $object->$key=$value;
+
+        return $object;
+    }
 }
