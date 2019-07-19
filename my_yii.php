09b4b9a30614aca7ad0aeaa498529582248ae3a8
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-07-20 04:36:03 +0900

    update phpdoc

--- my_yii.php
+++ my_yii.php
@@ -25,14 +25,7 @@ if(!class_exists('YiiBase', false)) {
 }
 
 /**
- * Yii is a helper class serving common framework functionalities.
- *
- * It encapsulates {@link YiiBase} which provides the actual implementation.
- * By writing your own Yii class, you can customize some functionalities of YiiBase.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @package system
- * @since 1.0
+ * Ray.Di powered Yii base class
  */
 class Yii extends YiiBase
 {
