923dc95dadaef2b6afb59ad6669d2b2de271d37a
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-07-20 02:26:04 +0900

    remove number of args condtion

--- my_yii.php
+++ my_yii.php
@@ -49,26 +49,9 @@ class Yii extends YiiBase
         if(!class_exists($type,false))
             $type=Yii::import($type,true);
 
-        if(($n=func_num_args())>1)
-        {
-            if($n===2)
-                $object=new $type($args[1]);
-            elseif($n===3)
-                $object=new $type($args[1],$args[2]);
-            elseif($n===4)
-                $object=new $type($args[1],$args[2],$args[3]);
-            else
-            {
-                unset($args[0]);
-                $class=new ReflectionClass($type);
-                // Note: ReflectionClass::newInstanceArgs() is available for PHP 5.1.3+
-                // $object=$class->newInstanceArgs($args);
-                $object=call_user_func_array(array($class,'newInstance'),$args);
-            }
-        }
-        else
-            $object=new $type;
-
+        unset($args[0]);
+        $class=new ReflectionClass($type);
+        $object=$class->newInstanceArgs($args);
         foreach($config as $key=>$value)
             $object->$key=$value;
 
