b4911364f0c435c62e8859d7bd9efc9f18a780f6
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-07-20 01:54:30 +0900

    add composer.json
    https://packagist.org/packages/yiisoft/yii

--- demos/helloworld/index.php
+++ demos/helloworld/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // include Yii bootstrap file
-require_once(dirname(__FILE__).'/../../framework/yii.php');
+require_once(dirname(__DIR__, 2) . '/vendor/yiisoft/yii/framework/yii.php');
 
 // create a Web application instance and run
 Yii::createWebApplication()->run();
