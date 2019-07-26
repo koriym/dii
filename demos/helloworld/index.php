2ae5b1deb556f2106cc575f39480b1eea144d859
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-07-26 19:37:30 +0900

    add logger

--- demos/helloworld/index.php
+++ demos/helloworld/index.php
@@ -2,6 +2,7 @@
 
 // include Yii bootstrap file
 require_once(dirname(__DIR__, 2) . '/yii_ray.php');
+$config=dirname(__FILE__).'/protected/config/main.php';
 
 // create a Web application instance and run
-Yii::createWebApplication()->run();
+Yii::createWebApplication($config)->run();
---
+++ demos/helloworld/protected/config/main.php
@@ -0,0 +1,27 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return [
+	// preloading 'log' component
+	'preload'=> ['log'],
+	// application components
+	'components'=> [
+		'log'=> [
+			'class'=>'CLogRouter',
+			'routes'=> [
+				[
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+                ],
+				// uncomment the following to show log messages on web pages
+				[
+					'class'=>'CWebLogRoute',
+                ],
+            ],
+        ],
+    ],
+];
