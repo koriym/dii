bccf8ac1e9665b7f66930c5203a66c758c8ba4d0
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-07-28 11:53:28 +0900

    remove autoload

--- yii_ray.php
+++ yii_ray.php
@@ -47,68 +47,6 @@ class Yii extends YiiBase
         return new Injector(new AppModule);
     }
 
-    public static function autoload($className, $classMapOnly = false)
-    {
-        foreach (self::$autoloaderFilters as $filter) {
-            if (is_array($filter)
-                && isset($filter[0]) && isset($filter[1])
-                && is_string($filter[0]) && is_string($filter[1])
-                && true === call_user_func([$filter[0], $filter[1]], $className)
-            ) {
-                return true;
-            } elseif (is_string($filter)
-                && true === call_user_func($filter, $className)
-            ) {
-                return true;
-            } elseif (is_callable($filter)
-                && true === $filter($className)
-            ) {
-                return true;
-            }
-        }
-
-        // use include so that the error PHP file may appear
-        if (isset(self::$classMap[$className])) {
-            include self::$classMap[$className];
-        } elseif (isset(self::$_coreClasses[$className])) {
-            include YII_PATH . self::$_coreClasses[$className];
-        } elseif ($classMapOnly) {
-            return false;
-        } else {
-            // include class file relying on include_path
-            if (strpos($className, '\\') === false) {  // class without namespace
-                if (self::$enableIncludePath === false) {
-                    foreach (self::$_includePaths as $path) {
-                        $classFile = $path . DIRECTORY_SEPARATOR . $className . '.php';
-                        if (is_file($classFile)) {
-                            include $classFile;
-                            if (YII_DEBUG && basename(realpath($classFile)) !== $className . '.php') {
-                                throw new CException(self::t('yii', 'Class name "{class}" does not match class file "{file}".', [
-                                    '{class}' => $className,
-                                    '{file}' => $classFile,
-                                ]));
-                            }
-                            break;
-                        }
-                    }
-                } else {
-                    include $className . '.php';
-                }
-            } else {  // class name with namespace in PHP 5.3
-                $namespace = str_replace('\\', '.', ltrim($className, '\\'));
-                if (($path = self::getPathOfAlias($namespace)) !== false && is_file($path . '.php')) {
-                    include $path . '.php';
-                } else {
-                    return false;
-                }
-            }
-
-            return class_exists($className, false) || interface_exists($className, false);
-        }
-
-        return true;
-    }
-
     /**
      * Extract config
      *
